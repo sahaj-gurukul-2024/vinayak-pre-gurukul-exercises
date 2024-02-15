d07963cd921fc46ccb7fc11844c7084c073493d7
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-15 16:30:03 +0530

    rename variables

--- Tambola_Validator/src/main/kotlin/WinnerValidator.kt
+++ Tambola_Validator/src/main/kotlin/WinnerValidator.kt
@@ -3,40 +3,42 @@ package org.example
 class WinnerValidator() {
     val rows = 3
     val column = 9
-    var topRow: MutableList<Int> = mutableListOf()
-    var BottomRow: MutableList<Int> = mutableListOf()
-    var anyFive: MutableList<Int> = mutableListOf()
-    var Status: String = "Rejected"
+    var topRow = mutableListOf<Int>()
+    var bottomRow = mutableListOf<Int>()
+    var firstFive = mutableListOf<Int>()
+    var status = "Rejected"
 
-    fun validateOneByOne(ticket: MutableList<MutableList<Int>>, numberAnounced: Int) {
+    fun validateOneByOne(ticket: MutableList<MutableList<Int>>, announcedNumber: Int) {
         for (i in 0 until rows) {
             for (j in 0 until column) {
-                val ticketval=ticket[i][j]
-                if (ticketval == numberAnounced) {
+                val ticketValue = ticket[i][j]
+                if (ticketValue == announcedNumber) {
                     if (i == 0) {
-                        topRow.add(numberAnounced)
+                        topRow.add(announcedNumber)
                     } else if (i == 2) {
-                        BottomRow.add(numberAnounced)
+                        bottomRow.add(announcedNumber)
                     } else {
-                        anyFive.add(numberAnounced)
+                        firstFive.add(announcedNumber)
                     }
                 }
             }
         }
     }
 
-    fun reciveTheInput(ticket: MutableList<MutableList<Int>>, numberSequence: MutableList<Int>, claim: String): String {
-        for (i in 0 until numberSequence.size) {
-            validateOneByOne(ticket, numberSequence[i])
-
+    fun reciveTheInput(ticket: MutableList<MutableList<Int>>, announcedNumberSequence: MutableList<Int>, claim: String): String {
+        for (i in 0 until announcedNumberSequence.size) {
+            validateOneByOne(ticket, announcedNumberSequence[i])
         }
-        val lastEntryInPut=numberSequence[numberSequence.size-1]
-        if(topRow.contains(lastEntryInPut)||BottomRow.contains(lastEntryInPut)||anyFive.contains(lastEntryInPut)){
-            if ((topRow.size == 5 && claim == "Top Row") || (BottomRow.size == 5 && claim == "Bottom Row") || (anyFive.size == 5 || claim == "Any Five")) {
-                Status = "Accepted"
-                return Status
+
+        val lastAnnouncedNumber = announcedNumberSequence[announcedNumberSequence.size-1]
+
+        if(topRow.contains(lastAnnouncedNumber)||bottomRow.contains(lastAnnouncedNumber)||firstFive.contains(lastAnnouncedNumber)){
+            if ((topRow.size == 5 && claim == "Top Row") || (bottomRow.size == 5 && claim == "Bottom Row") || (firstFive.size == 5 || claim == "Any Five")) {
+                status = "Accepted"
+                return status
             }
         }
-        return Status
+
+        return status
     }
 }
