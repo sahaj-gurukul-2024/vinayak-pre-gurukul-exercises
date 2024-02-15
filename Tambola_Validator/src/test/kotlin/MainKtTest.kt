ed1d321212bcd8e31179ff1533abd111ef9982f8
V-N-Bhat <whiterover31@outlook.com>
2024-02-15 11:13:00 +0530

    time machine

---
+++ Tambola_Validator/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,16 @@
+import org.example.WinnerValidator
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MainKtTest(){
+    @Test
+    fun `Validate the top row`(){
+        val tambolaBoard: MutableList<MutableList<Int>> = mutableListOf(
+            mutableListOf(4,16,0,0,48,0,63,76,0),
+            mutableListOf(7,0,23,38,0,52,0,0,80),
+            mutableListOf(9,0,25,0,0,56,64,0,83)
+        )
+        val number:MutableList<Int> = mutableListOf(90, 4, 46, 63, 89, 16, 76, 48 )
+        val validate=WinnerValidator()
+    }
+}
