e02ba2f7473919f6c295d2cf7a6cf35a0de30e85
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-15 17:00:47 +0530

    rename test variables and class name

--- Tambola_Validator/src/test/kotlin/WinnerValidatorTest.kt
+++ Tambola_Validator/src/test/kotlin/WinnerValidatorTest.kt
@@ -2,29 +2,33 @@ import org.example.ClaimValidator
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-class MainKtTest(){
+class ClaimValidatorTest{
     @Test
     fun `Validate the top row`(){
-        val tambolaBoard: MutableList<MutableList<Int>> = mutableListOf(
+        val ticket = mutableListOf(
             mutableListOf(4,16,0,0,48,0,63,76,0),
             mutableListOf(7,0,23,38,0,52,0,0,80),
             mutableListOf(9,0,25,0,0,56,64,0,83)
         )
-        val numberList:MutableList<Int> = mutableListOf(90, 4, 46, 63, 89, 16, 76, 48 )
-        val validate=ClaimValidator()
-        val result=validate.validateClaim(tambolaBoard, numberList,"Top Row")
-        assertEquals("Accepted",result)
+        val numberList = mutableListOf(90, 4, 46, 63, 89, 16, 76, 48 )
+        val validator = ClaimValidator()
+
+        val result = validator.validateClaim(ticket, numberList,"Top Row")
+
+        assertEquals("Accepted", result)
     }
     @Test
     fun `failing of the Top row claim not valid`(){
-        val tambolaBoard: MutableList<MutableList<Int>> = mutableListOf(
+        val ticket = mutableListOf(
             mutableListOf(4,16,0,0,48,0,63,76,0),
             mutableListOf(7,0,23,38,0,52,0,0,80),
             mutableListOf(9,0,25,0,0,56,64,0,83)
         )
-        val numberList:MutableList<Int> = mutableListOf(90, 4, 46, 63, 89, 16, 76, 48, 12 )
-        val validate=ClaimValidator()
-        val result=validate.validateClaim(tambolaBoard, numberList,"Top Row")
-        assertEquals("Rejected",result)
+        val numberList = mutableListOf(90, 4, 46, 63, 89, 16, 76, 48, 12 )
+        val validator = ClaimValidator()
+
+        val result = validator.validateClaim(ticket, numberList,"Top Row")
+
+        assertEquals("Rejected", result)
     }
 }
