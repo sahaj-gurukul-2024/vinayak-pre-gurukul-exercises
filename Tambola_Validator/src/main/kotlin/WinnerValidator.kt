0ec72de98732b3f187cdb6d09020032a77ccc623
V-N-Bhat <whiterover31@outlook.com>
2024-02-15 10:45:34 +0530

    just to save not more than that

--- Tambola_Validator/src/main/kotlin/WinnerValidator.kt
+++ Tambola_Validator/src/main/kotlin/WinnerValidator.kt
@@ -1,8 +1,31 @@
 package org.example
 
 class WinnerValidator(){
-    val list = mutableListOf<MutableList<Int>>()
-    fun reciveTheInput(){
+    val rows=3
+    val column=9
+    val topRow:MutableList<Int> = mutableListOf()
+    val BottomRow:MutableList<Int> = mutableListOf()
+    val anyFive:MutableList<Int> = mutableListOf()
+    //val twoDArray: MutableList<MutableList<Int>> = MutableList(rows) { MutableList(column) { 0 } }
 
+    fun validateOneByOne(ticket: MutableList<MutableList<Int>>, numberAnounced:Int){
+        for(i in 0 until rows){
+            for (j in 0 until rows) {
+                if(ticket[i][j]==numberAnounced){
+                    if(i==0){
+                        topRow.add(numberAnounced)
+                    }
+                    else if(i==2){
+                        BottomRow.add(numberAnounced)
+                    }
+                    else{
+                        anyFive.add(numberAnounced)
+                    }
+                }
+            }
+    }
+    fun reciveTheInput(ticket:MutableList<MutableList<Int>>,numberSequence:MutableList<Int>){
+
+        }
     }
 }
